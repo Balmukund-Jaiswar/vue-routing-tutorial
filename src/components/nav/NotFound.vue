0ef9164753cb687d87b924fa44398eedee6cceb0
Mukund-Jaiswar <mukundjaiswar2001@gmail.com>
2023-02-14 18:53:32 +0530

    Intial commit
    Vue routing fundamentals

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,4 @@
+<template>
+    <p>Not found your requested page! visit teams below</p>
+    <router-link to="/teams">Teams</router-link>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,33 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue'
+import UsersFooter from './components/users/UsersFooter.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    { path: '/teams', components:{default:TeamsList,footer:TeamsFooter}, children:
+        [{name:'team-members',path:':teamId', component: TeamMembers, props: true}]}, // our-domain.com/teams => TeamsList
+    { path: '/users', components: {default:UsersList,footer:UsersFooter} },
+    { path: '/:notFound(.*)', component: NotFound }
+  ],
+  scrollBehavior(to,from,savedPosition){
+    if(savedPosition){
+        return savedPosition
+    }
+    return {left:0,top:0}
+  }
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
